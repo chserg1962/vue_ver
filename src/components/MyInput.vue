636462f952da4b2cfea1baddbdbc59ecd9fb8214
c_serg@mail.ru <c_serg>
2020-09-26 18:28:58 +0300

    Перевод теста программ на композиционный стиль

---
+++ src/components/MyInput.vue
@@ -0,0 +1,31 @@
+// vue/no-mutating-props
+<template>
+  <div>
+    <input :value="aValue" @input="$emit('input', $event.target.value)" @click="openModal">
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['input', 'test'],
+  setup() {
+    const aValue = ref('пример');
+
+    function openModal() {
+      this.$emit('input', 'test');
+    }
+
+    return {
+      aValue,
+      openModal,
+    };
+  },
+};
+</script>
